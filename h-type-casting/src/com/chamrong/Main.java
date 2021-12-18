b6b13bdef19224b7cb0b7b7635b5021cddaa92b5
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-18 17:44:19 +0700

    case cading

---
+++ h-type-casting/src/com/chamrong/Main.java
@@ -0,0 +1,13 @@
+package com.chamrong;
+
+public class Main {
+
+  public static void main(String[] args) {
+    int age = Integer.parseInt("10");
+    displayAge(age);
+  }
+
+  private static void displayAge(int age) {
+    System.out.println("Age: " + age);
+  }
+}
