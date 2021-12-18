ad051ba66250f1dbeb47861e002ef5cdee7e0c3b
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-18 17:59:31 +0700

    if else if else

---
+++ i-comparison-and-logical-operators/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+J
+src/com/chamrong/Main.java,e\9\e9913ca373d9eaa3da0d72f78d08ff590fd3eb65
---
+++ j-if-else-statement/src/com/chamrong/Main.java
@@ -0,0 +1,27 @@
+package com.chamrong;
+
+import java.util.Objects;
+
+public class Main {
+
+  public static void main(String[] args) {
+    printIf("a");
+    printIfElse("b");
+    printElseIfElse("c");
+  }
+
+  private static void printElseIfElse(String term) {
+    if (Objects.equals(term, "c")) System.out.println("a == " + term);
+    else if (Objects.equals(term, "a")) System.out.println("b == " + term);
+    else System.out.println("nothing!");
+  }
+
+  private static void printIfElse(String term) {
+    if (Objects.equals(term, "a")) System.out.println("a == " + term);
+    else System.out.println("a == " + term);
+  }
+
+  private static void printIf(String term) {
+    if (Objects.equals(term, "a")) System.out.println("a = " + term);
+  }
+}
