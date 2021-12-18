e9f25c95452a49dec4a572e0818433bf53b6c9de
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-18 18:19:22 +0700

    array

---
+++ n-array/src/com/chamrong/Main.java
@@ -0,0 +1,10 @@
+package com.chamrong;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] words = {"love", "life"};
+        for(int i = 0; i < 2; i++)
+            System.out.println(words[i]);
+    }
+}
