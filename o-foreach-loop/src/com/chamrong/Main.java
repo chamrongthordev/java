8ca8b04f2492f4315e1f4790487b4fd355be7ddc
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-18 18:21:09 +0700

    foreach

---
+++ o-foreach-loop/src/com/chamrong/Main.java
@@ -0,0 +1,12 @@
+package com.chamrong;
+
+public class Main {
+
+  public static void main(String[] args) {
+    String[] words = {"love", "life"};
+
+    for (String word : words) {
+      System.out.println(word);
+    }
+  }
+}
