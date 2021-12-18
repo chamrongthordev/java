3219b7362baf3ecda8acc7d71b4c579bacc20cc2
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-18 15:22:02 +0700

    variable

---
+++ a-hello-world/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+C
+src/HelloWorld.java,f\8\f8f6ccab2f71fc7c0dd6b506e31cb856f7a2fa2d
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
---
+++ b-variable/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import static java.lang.System.out;
+
+public class Main {
+
+  public static void main(String[] args) {
+    byte byteNumber = 1;
+    short shortNumber = 1;
+    int intNumber = 1;
+    float floatNumber = 1.0F;
+    double doubleNumber = 2.0;
+
+    String stringVariable = "hello, world!";
+
+    out.println("byteNumber: " + byteNumber);
+    out.println("shortNumber: " + shortNumber);
+    out.println("intNumber: " + intNumber);
+    out.println("floatNumber: " + floatNumber);
+    out.println("doubleNumber: " + doubleNumber);
+    out.println("stringNumber: " + stringVariable);
+  }
+}
