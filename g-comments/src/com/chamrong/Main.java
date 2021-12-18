56f78072bba423488304f34e00f3a56804508d63
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-18 17:38:36 +0700

    comment

---
+++ g-comments/src/com/chamrong/Main.java
@@ -0,0 +1,19 @@
+package com.chamrong;
+
+public class Main {
+
+    public static void main(String[] args) {
+	      // variable
+        int age = 20;
+        
+        displayAge(age);
+    }
+
+    /**
+     * to display age.
+     * @param age
+     */
+    private static void displayAge(int age) {
+        System.out.println("age: " + age);
+    }
+}
