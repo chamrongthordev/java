47007f16269e8e3edf1938eb181afa0a4973eb7f
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-18 18:49:53 +0700

    static keyword

---
+++ t-static-keyword/src/com/chamrong/Main.java
@@ -0,0 +1,12 @@
+package com.chamrong;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+
+    public static void printHelloWorld() {
+    System.out.println("Hello, World!");
+    }
+}
