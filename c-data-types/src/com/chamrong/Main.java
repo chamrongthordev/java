e16107c1be4c3cbcd2d6f3cfb25324a573b73a4f
chamrongthordev <chamrongthor.dev@gmail.com>
2021-12-18 17:16:35 +0700

    data type

---
+++ c-data-types/src/com/chamrong/Main.java
@@ -0,0 +1,15 @@
+package com.chamrong;
+
+public class Main {
+
+    public static void main(String[] args) {
+	      int age = 45;
+        int userId = 45;
+        double firstNumber = 64.33;
+        float secondNumber = 64.33f;
+        double factor = 22.44e6;
+        char alpabet = 'a';
+        boolean result1 = true;
+        boolean result2 = false;
+    }
+}
